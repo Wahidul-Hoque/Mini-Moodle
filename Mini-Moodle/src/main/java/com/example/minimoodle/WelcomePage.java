62a730196b85fd12414497513672760d13d9a097
Mehedi Hasan Kanon <mehedi.hkanon@gmail.com>
2025-07-08 17:09:00 +0600

    Add Welcome Page UI and controller for Mini-Moodle
    Introduced WelcomePage and WelcomePageController Java classes, along with a corresponding FXML layout for the welcome page. The new UI allows users to select their role (Admin or Teacher) and proceed to login. Minor cleanup in HelloApplication.

---
+++ Mini-Moodle/src/main/java/com/example/minimoodle/WelcomePage.java
@@ -0,0 +1,25 @@
+package com.example.minimoodle;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+
+
+public class WelcomePage extends Application {
+
+    @Override
+    public void start(Stage stage)
+    {
+        
+    }
+
+
+    public static void main(String[] args) {
+        launch();
+    }
+
+}
---
+++ Mini-Moodle/src/main/java/com/example/minimoodle/WelcomePageController.java
@@ -0,0 +1,26 @@
+package com.example.minimoodle;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
+
+public class WelcomePageController {
+
+    @FXML
+    private ToggleGroup roleGroup;
+
+    @FXML
+    private RadioButton welcomePageAdminRadioButton;
+
+    @FXML
+    private RadioButton welcomePageUserRadioButton;
+
+    @FXML
+    private Button welcomePageContinueButton;
+
+
+
+
+
+}
